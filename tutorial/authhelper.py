4711b9022b71d5a109165188774302a30c106ceb
Bhairav Mehta <bhairavmehta95@gmail.com>
2016-08-05 15:36:54 -0500

    app permissions rmeoved

--- tutorial/authhelper.py
+++ tutorial/authhelper.py
@@ -23,9 +23,7 @@ token_url = '{0}{1}'.format(authority, '/common/oauth2/v2.0/token')
 # The scopes required by the app
 scopes = [ 'openid',
            'profile',
-           'https://outlook.office.com/mail.read',
            'https://outlook.office.com/calendars.readwrite',
-           'https://outlook.office.com/contacts.read',
 ]
 
 def get_signin_url(redirect_uri):
